797041dc47de8e836065d24b900163a0878b096b
Lucas Verneck <lpverneck@gmail.com>
2023-10-22 18:03:00 -0300

    feat: initialize repo configs

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,10 @@
+import re
+import sys
+
+REGEX_PATTERN = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+
+project_name = "{{ cookiecutter.project_name }}"
+
+if not re.match(REGEX_PATTERN, project_name):
+    print("ERROR: {project_name} is not a valid project name !")
+    sys.exit(1)
---
+++ {{ cookiecutter.project_name }}/main.py
@@ -0,0 +1,1 @@
+print("Hello World, {{ cookiecutter.project_name}} !")
